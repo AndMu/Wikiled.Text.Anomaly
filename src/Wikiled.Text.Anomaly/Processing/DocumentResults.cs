47b67b3cc0f27866300f1f355426b8ce2f5a044f
Andrius Mudinas <keistokas@gmail.com>
2018-07-19 21:01:55 +0100

    Update

---
+++ src/Wikiled.Text.Anomaly/Processing/DocumentResults.cs
@@ -0,0 +1,18 @@
+﻿using Wikiled.Text.Analysis.Structure;
+using Wikiled.Text.Anomaly.Processing.Filters;
+
+namespace Wikiled.Text.Anomaly.Processing
+{
+    public class DocumentResults
+    {
+        public DocumentResults(Document document, TextCluster[] clusters)
+        {
+            Clusters = clusters;
+            Document = document;
+        }
+
+        public Document Document { get; }
+
+        public TextCluster[]Clusters { get; }
+    }
+}
